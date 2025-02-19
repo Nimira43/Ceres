e1fe558d0babbe8ca666dda651df24cad519dc81
Nimira43 <n.rabone72@aol.com>
2025-02-19 23:45:17 +0000

    Tailwind working fine. Brought in Great Vibes font for logo text.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function page() {
   return (
-    <div>NatuFood</div>
+    <div>
+      <h1 className='text-4xl logo p-5 text-[#00ff00] bg-[#a0522d]'>NatuFood</h1>
+    </div>
   )
 }
