f8b4518688c963a60d8e82e0e3f6b6aec81a1537
Nimira43 <n.rabone72@aol.com>
2025-02-20 04:31:13 +0000

    Brought in Input component from Shadcn.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import ButtonSpecials from '@/components/buttons/buttonSpecials'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 export default function page() {
   return (
     <div className='bg-light'>
       <h1 className='text-4xl logo p-5 text-main-dark bg-main-light font-semibold'>Ceres</h1>
-      <div className="p-4">
-        <ButtonSpecials variant="primary">Login</ButtonSpecials>
-        <ButtonSpecials variant="secondary">Register</ButtonSpecials>
-        <ButtonSpecials variant="accent">Submit</ButtonSpecials>
-        <ButtonSpecials variant="disabled" disabled>Edit Profile</ButtonSpecials>
+      <div className='p-4'>
+        <ButtonSpecials variant='primary'>Login</ButtonSpecials>
+        <ButtonSpecials variant='secondary'>Register</ButtonSpecials>
+        <ButtonSpecials variant='accent'>Submit</ButtonSpecials>
+        <ButtonSpecials variant='disabled' disabled>Edit Profile</ButtonSpecials>
         <Button>Shadcn</Button>
+        <Input placeholder='Search...'/>
       </div>
     </div>      
   )
