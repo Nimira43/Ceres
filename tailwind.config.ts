49a7db77a2ce46d891775ca3baae2b7ba6af0d55
Nimira43 <n.rabone72@aol.com>
2025-02-20 04:09:25 +0000

    Brought  in Shadcn UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,74 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
+    darkMode: ['class'],
+    content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        'main-dark': '#004d00',
-        'main': '#0bb80b',
-        'main-light': '#a3f1a3',
-        'support-dark': '#3150ab',
-        'support': '#4169e1',
-        'accent-dark': '#c4a704',
-        'accent': '#ffd700',
-        'dark': '#111',
-        'light': '#fefffe',
-        'grey-dark': '#333',
-        'grey-medium': '#999',
-        'grey-light': '#ccc',
-        'grey-light-extra': '#eee',
-      },
-    },
+  	extend: {
+  		colors: {
+  			'main-dark': '#004d00',
+  			main: '#0bb80b',
+  			'main-light': '#a3f1a3',
+  			'support-dark': '#3150ab',
+  			support: '#4169e1',
+  			'accent-dark': '#c4a704',
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			dark: '#111',
+  			light: '#fefffe',
+  			'grey-dark': '#333',
+  			'grey-medium': '#999',
+  			'grey-light': '#ccc',
+  			'grey-light-extra': '#eee',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config
