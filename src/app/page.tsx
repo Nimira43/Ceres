4c1f942d838a312a822095fa2b93f2bc362531a4
Nimira43 <n.rabone72@aol.com>
2025-02-27 22:56:03 +0000

    Removed the special buttons components - saved to my dev folder for future use elsewhere.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import ButtonSpecials from '@/components/buttons/buttonSpecials'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
@@ -7,10 +6,6 @@ export default function page() {
     <div className='bg-light'>
       <h1 className='text-4xl logo p-5 text-main bg-gold font-semibold'>Ceres</h1>
       <div className='p-4'>
-        <ButtonSpecials variant='primary'>Login</ButtonSpecials>
-        <ButtonSpecials variant='secondary'>Register</ButtonSpecials>
-        <ButtonSpecials variant='accent'>Submit</ButtonSpecials>
-        <ButtonSpecials variant='disabled' disabled>Edit Profile</ButtonSpecials>
         <Button>Shadcn</Button>
         <Input placeholder='Search...'/>
       </div>
