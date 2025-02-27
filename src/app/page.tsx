84596b70c52441fbf994b5376cda068aad75d110
Nimira43 <n.rabone72@aol.com>
2025-02-27 23:18:19 +0000

    Basic navbar items displayed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,16 @@ function HomePage() {
       <div className='lg:px-20 px-5 py-10 bg-gold-light'>
         <div className='flex justify-between '>
           <h1 className='font-bold text-3xl text-primary logo'>Ceres</h1>
-          <div className='flex gap-5'></div>
+          <div className='flex gap-5'>
+            {menuItems.map((item, index) => (
+              <div
+                key={index}
+                className='text-sm text-main font-medium'
+              >
+                {item.name}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
