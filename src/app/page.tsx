49943b8f031b42f9cf4e52ec35bff139894d529c
Nimira43 <n.rabone72@aol.com>
2025-03-13 17:02:52 +0000

    Setting out grid layout for hero section of page.tsx.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,22 +17,23 @@ function HomePage() {
   ]
 
   return (
-    <div>
-      <div className='lg:px-20 px-5 py-10'>
-        <div className='flex justify-between items-center'>
-          <h1 className='font-bold text-4xl text-primary logo'>Ceres</h1>
-          <div className='flex gap-5 items-center'>
-            {menuItems.map((item, index) => (
-              <div
-                key={index}
-                className='text-sm text-main hover:text-main-dark font-medium uppercase transition-colors duration-500 cursor-pointer'
-              >
-                {item.name}
-              </div>
-            ))}
-            <Button className='uppercase'>Login</Button>
-          </div>
+    <div className='lg:px-20 px-5 py-10'>
+      <div className='flex justify-between items-center'>
+        <h1 className='font-bold text-4xl text-primary logo'>Ceres</h1>
+        <div className='flex gap-5 items-center'>
+          {menuItems.map((item, index) => (
+            <div
+              key={index}
+              className='text-sm text-main hover:text-main-dark font-medium uppercase transition-colors duration-500 cursor-pointer'
+            >
+              {item.name}
+            </div>
+          ))}
+          <Button className='uppercase'>Login</Button>
         </div>
+      </div>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+
       </div>
     </div>
   )
