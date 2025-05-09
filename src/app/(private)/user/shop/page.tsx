433b1ed554f0d939dd2e916bab77d4d850783fdb
Nimira43 <n.rabone72@aol.com>
2025-05-09 22:20:42 +0100

    Brought in UserButton

--- src/app/(private)/user/shop/page.tsx
+++ src/app/(private)/user/shop/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { UserButton } from '@clerk/nextjs'
 
 function UserShop() {
   return (
-    <div>
-      UserShop
+    <div className='p-5 flex flex-col gap-5'>
+      <h1>Cere | Shop</h1>
+      <UserButton />
     </div>
   )
 }
