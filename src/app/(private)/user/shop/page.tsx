b3c25b6469ccff4479b6c0e33b28807cb531babf
Nimira43 <n.rabone72@aol.com>
2025-05-09 22:23:29 +0100

    Styled h1

--- src/app/(private)/user/shop/page.tsx
+++ src/app/(private)/user/shop/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from '@clerk/nextjs'
 function UserShop() {
   return (
     <div className='p-5 flex flex-col gap-5'>
-      <h1>Cere | Shop</h1>
+      <h1 className='font-bold text-4xl text-primary logo'>Ceres</h1>
       <UserButton />
     </div>
   )
