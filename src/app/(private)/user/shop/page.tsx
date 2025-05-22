5158c0759d77ad3df27ae60c9aa931aab9be4748
Nimira43 <n.rabone72@aol.com>
2025-05-22 04:08:39 +0100

    Getting the Clerk user Id. Moved the Clerk middleware.tsx file to src.

--- src/app/(private)/user/shop/page.tsx
+++ src/app/(private)/user/shop/page.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server'
 
-function UserShop() {
+async function UserShop() {
+  const user = await currentUser()
+  console.log(user)
+
   return (
     <div className='p-5 flex flex-col gap-5'>
       <h1 className='font-bold text-4xl text-primary logo'>Ceres</h1>
       <UserButton />
+      <div className='flex flex-col gap-2'>
+        <h1>CLerk User ID: {user?.id}</h1>
+      </div>
     </div>
   )
 }
 
 export default UserShop
-UserShop
